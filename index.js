5380c5fb5d0e756de73da00206c86c12aa72deba
Prajwalp111 <prajwalp1812@gmail.com>
2025-05-14 00:57:21 +0530

    new project which includes mongoDB

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+
+app.get('/dog', (req, res)=>{
+    res.send('woof!!')
+})
+
+app.listen(8080, ()=>{
+    console.log('listening on port 8080')
+})
